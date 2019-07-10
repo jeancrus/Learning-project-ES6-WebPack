56d8081ce216bd2cfa9ab089643bbb5fa69df685
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-10 18:01:56 -0300

    Finishing project, adding onload and treating promise error from axios

--- src/main.js
+++ src/main.js
@@ -16,6 +16,17 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event)
     }
 
+    setLoading(loading = true) {
+        if (loading) {
+            let loadingEl = document.createElement('span')
+            loadingEl.appendChild(document.createTextNode('Carregando...'))
+            loadingEl.setAttribute('id', 'loading')
+            this.formEl.insertAdjacentElement('afterend', loadingEl)
+        } else {
+            document.getElementById('loading').remove()
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault()
 
@@ -23,18 +34,25 @@ class App {
 
         if (!repoInput) return
 
-        const response = await api.get(`/repos/${repoInput}`)
+        try {
+            const response = await api.get(`/repos/${repoInput}`)
 
-        const { name, description, html_url, owner: { avatar_url } } = response.data
+            this.setLoading()
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        })
-        this.inputEl.value = ''
-        this.render()
+            const { name, description, html_url, owner: { avatar_url } } = response.data
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            })
+            this.inputEl.value = ''
+            this.render()
+        } catch (error) {
+            alert('O repositório não existe')
+        }
+        this.setLoading(false)
     }
 
     render() {
